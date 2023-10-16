2842472cdcbdb87892bdda3d12aa7fff4a37261f
Julian Betancur <julianbetancur@gmail.com>
2023-10-16 07:51:01 +0200

    add diffdrr

---
+++ test_diffdrr.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import torch
+
+from diffdrr.drr import DRR
+from diffdrr.data import load_example_ct
+from diffdrr.visualization import plot_drr
+
+# Read in the volume and get the isocenter
+volume, spacing = load_example_ct()
+bx, by, bz = torch.tensor(volume.shape) * torch.tensor(spacing) / 2
+
+# Initialize the DRR module for generating synthetic X-rays
+device = "cuda" if torch.cuda.is_available() else "cpu"
+drr = DRR(
+    volume,      # The CT volume as a numpy array
+    spacing,     # Voxel dimensions of the CT
+    sdr=300.0,   # Source-to-detector radius (half of the source-to-detector distance)
+    height=200,  # Height of the DRR (if width is not seperately provided, the generated image is square)
+    delx=4.0,    # Pixel spacing (in mm)
+).to(device)
+
+# Set the camera pose with rotation (yaw, pitch, roll) and translation (x, y, z)
+rotation = torch.tensor([[torch.pi, 0.0, torch.pi / 2]], device=device)
+translation = torch.tensor([[bx, by, bz]], device=device)
+
+# 📸 Also note that DiffDRR can take many representations of SO(3) 📸
+# For example, quaternions, rotation matrix, axis-angle, etc...
+img = drr(rotation, translation, parameterization="euler_angles", convention="ZYX")
+plot_drr(img, ticks=False)
+plt.show()
